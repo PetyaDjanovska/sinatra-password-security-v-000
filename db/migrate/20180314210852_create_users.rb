4b2736ca5581497525fb7ce626e23150bffaa23d
Petya Djanovska <petya.djanovska@gmail.com>
2018-05-31 18:05:12 +0000

    automatically backed up by learn

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,8 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
-  def change
+  def up
     create_table :users do |t|
       t.string :username
       t.string :password
-    end
+  end
+
+  def down
   end
 end
